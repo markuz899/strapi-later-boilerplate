f8288c075937d5c64c3e3a2caf60bbb51014ea38
markuz899 <markuz.89@hotmail.it>
2024-06-30 19:26:36 +0200

    [feat] - upgrade version

--- .strapi/client/app.js
+++ .strapi/client/app.js
@@ -5,6 +5,9 @@
 import strapiCloud from "@strapi/plugin-cloud/strapi-admin";
 import i18N from "@strapi/plugin-i18n/strapi-admin";
 import usersPermissions from "@strapi/plugin-users-permissions/strapi-admin";
+import strapiAdvancedUuid from "strapi-advanced-uuid/strapi-admin";
+import configSync from "strapi-plugin-config-sync/strapi-admin";
+import importExportEntries from "strapi-plugin-import-export-entries/strapi-admin";
 import { renderAdmin } from "@strapi/strapi/admin";
 
 renderAdmin(document.getElementById("strapi"), {
@@ -12,5 +15,8 @@ renderAdmin(document.getElementById("strapi"), {
     "strapi-cloud": strapiCloud,
     i18n: i18N,
     "users-permissions": usersPermissions,
+    "strapi-advanced-uuid": strapiAdvancedUuid,
+    "config-sync": configSync,
+    "import-export-entries": importExportEntries,
   },
 });
